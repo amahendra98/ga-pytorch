e4e053f41e25fbc520dc87ee673cfe81f7ac9674
mlmyd <yang.deng@duke.edu>
2020-08-24 01:48:25 -0400

    Fixing differences

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ def main(argv):
 
     parser.add_argument('--device', type=str, help='Device list for model storage', default = ['cuda:1'])
 
-    parser.add_argument('--pop-size', type=int, help='Population size.', default=100)
+    parser.add_argument('--pop-size', type=int, help='Population size.', default=2000)
 
-    parser.add_argument('--generations', type=int, default=52, metavar='N',
+    parser.add_argument('--generations', type=int, default=1002, metavar='N',
                             help='number of generations to train (default: 1000)')
 
     parser.add_argument('--top', type=int, default=0, metavar='N',
                             help='numer of top elites that should be re-evaluated')
 
-    parser.add_argument('--trunc-threshold', type=int, default=.01,
+    parser.add_argument('--trunc-threshold', type=int, default=.015,
                             help='Fraction of models that survive each generation')
 
     parser.add_argument('--mutation-power', type=float, help="Mutation Power", default = 0.02)
