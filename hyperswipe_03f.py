96ccd3273dbe3dd4c91c7b809ab171ca2d85bcc8
mlmyd <yang.deng@duke.edu>
2020-10-27 04:55:25 -0400

    Modifying hyperswipe

--- hyperswipe_03f.py
+++ hyperswipe_03f.py
@@ -14,11 +14,12 @@ if __name__ == '__main__':
 
     f = flag_reader.read_flag()
     t = f.trunc_threshold
-    f.mutation_power = 0.02
+    f.mutation_power = 0.05
     m = f.mutation_power
     p = f.pop_size
+    f.schedule_args = ('generational scheduler', 10, [(0.05,0), (0.02,20),(0.01,150)])
 
-    f.folder = f.folder + "/T{}_M{}_P{}_100by4".format(t,m,p)
+    f.folder = f.folder + "/T{}_M{}_P{}_{}".format(t,m,p,f.schedule_args[0])+'_run4'
     f.device = ['cuda:0']
     print(f)
     ga = GA(f)
