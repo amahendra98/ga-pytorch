e4560031dc2e99916b20b6459260d4346b01546b
mlmyd <yang.deng@duke.edu>
2020-10-31 23:37:30 -0400

    Swipe Mods: Extra Value Scheduler

--- hyperswipe_03c.py
+++ hyperswipe_03c.py
@@ -1,4 +1,5 @@
 from ga import GA
+import os
 import flag_reader
 
 if __name__ == '__main__':
@@ -7,35 +8,39 @@ if __name__ == '__main__':
     Trunc = [0.001, 0.01, 0.1, 0.2,0.3,0.4]
     Pop = [5000,4000,3000,1000]
 
-    History = None
-    with open('sweep_04_dirs.txt','r') as f:
-        History = eval(f.read())
-
     for l in Layers:
         for n in Nodes:
             for t in Trunc:
                 for p in Pop:
-                    f = flag_reader.read_flag()
-                    f.mutation_power = 0.01
-                    m = f.mutation_power
-                    name = 'T{}_M{}_P{}_L{}_N{}'.format(t,m,p,l,n)
+                    for c in range(2):
+                        f = flag_reader.read_flag()
+                        f.mutation_power = 0.01
+                        m = f.mutation_power
 
-                    if name in History:
-                        continue
+                        with open('sweep_07_dirs.txt', 'r') as f:
+                            History = eval(f.read())
 
-                    f.pop_size = p
-                    f.trunc_threshold = t
-                    f.linear = [n for j in range(l + 2)]
-                    f.linear[0] = 2
-                    f.linear[-1] = 300
-                    f.schedule_args = [None, None]
-                    f.generations = 500
-                    f.device = ['cuda:0']
-                    f.folder = '/work/amm163/results/sweep_04/'+name
+                        name = 'P{}_T{}_value_scheduler_{}'.format(p,t,c)
 
-                    print(f)
-                    ga = GA(f)
-                    ga.run()
+                        if name in History:
+                            continue
+
+                        History.append(name)
+
+                        f.pop_size = p
+                        f.trunc_threshold = t
+                        f.linear = [n for j in range(l + 2)]
+                        f.linear[0] = 2
+                        f.linear[-1] = 300
+                        f.schedule_args = [None, None]
+                        f.generations = 500
+                        f.device = ['cuda:0']
+                        f.folder = '/work/amm163/results/sweep_07/'+name
+
+                        print(f)
+                        ga = GA(f)
+                        ga.run()
+
 
     ''' 
     Small_1 = [5,5,5,5,5,5,5]
--- hyperswipe_03f.py
+++ hyperswipe_03f.py
@@ -1,7 +1,48 @@
 from ga import GA
+import os
 import flag_reader
 
 if __name__ == '__main__':
+    Layers = [1,2,3,4]
+    Nodes = [50,100,200,500]
+    Trunc = [0.001, 0.01, 0.1, 0.2,0.3,0.4]
+    Pop = [5000,4000,3000,1000]
+
+    for l in Layers:
+        for n in Nodes:
+            for t in Trunc:
+                for p in Pop:
+                    for c in range(2):
+                        f = flag_reader.read_flag()
+                        f.mutation_power = 0.01
+                        m = f.mutation_power
+
+                        with open('sweep_07_dirs.txt', 'r') as f:
+                            History = eval(f.read())
+
+                        name = 'P{}_T{}_value_scheduler_{}'.format(p,t,c)
+
+                        if name in History:
+                            continue
+
+                        History.append(name)
+
+                        f.pop_size = p
+                        f.trunc_threshold = t
+                        f.linear = [n for j in range(l + 2)]
+                        f.linear[0] = 2
+                        f.linear[-1] = 300
+                        f.schedule_args = [None, None]
+                        f.generations = 500
+                        f.device = ['cuda:0']
+                        f.folder = '/work/amm163/results/sweep_07/'+name
+
+                        print(f)
+                        ga = GA(f)
+                        ga.run()
+
+
+    '''
     Layers = [1,2,3,4]
     Nodes = [50,100,200,500]
     Trunc = [0.001, 0.01, 0.1, 0.2,0.3,0.4]
@@ -36,6 +77,7 @@ if __name__ == '__main__':
                     print(f)
                     ga = GA(f)
                     ga.run()
+    '''
 
     '''
     Trunc = [0.01]
