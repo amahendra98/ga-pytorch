51145a516cf8a040526283694c864ff82a7ecbbd
mlmyd <yang.deng@duke.edu>
2020-11-01 03:53:32 -0500

    Hyperwsipe Mods: Fixing

--- hyperswipe_03d.py
+++ hyperswipe_03d.py
@@ -3,44 +3,40 @@ import os
 import flag_reader
 
 if __name__ == '__main__':
-    Layers = [1,2,3,4]
-    Nodes = [100]
     Trunc = [0.001, 0.01, 0.1, 0.2,0.3,0.4]
-    Pop = [5000,4000,3000,1000]
+    Pop = [3000,1000]
 
-    for l in Layers:
-        for n in Nodes:
-            for t in Trunc:
-                for p in Pop:
-                    for c in range(2):
-                        f = flag_reader.read_flag()
-                        f.mutation_power = 0.01
-                        m = f.mutation_power
+    for t in Trunc:
+        for p in Pop:
+            for c in range(2):
+                f = flag_reader.read_flag()
+                f.mutation_power = 0.01
+                m = f.mutation_power
 
                         #with open('sweep_07_dirs.txt', 'r') as f:
                         #    History = eval(f.read())
 
-                        name = 'P{}_T{}_value_scheduler_{}'.format(p,t,c)
+                name = 'P{}_T{}_value_scheduler_{}'.format(p,t,c)
 
-                        '''
-                        if name in History:
-                            continue
+                '''
+                if name in History:
+                    continue
 
-                        History.append(name)
-                        with open('sweep_07_dirs.txt', 'w') as f:
-                            f.truncate()
-                            print(History, file=f)
-                        '''
+                History.append(name)
+                with open('sweep_07_dirs.txt', 'w') as f:
+                    f.truncate()
+                    print(History, file=f)
+                '''
 
-                        f.pop_size = p
-                        f.trunc_threshold = t
-                        f.generations = 200
-                        f.device = ['cuda:0']
-                        f.folder = '/work/amm163/results/sweep_07/'+name
+                f.pop_size = p
+                f.trunc_threshold = t
+                f.generations = 200
+                f.device = ['cuda:0']
+                f.folder = '/work/amm163/results/sweep_07/'+name
 
-                        print(f)
-                        ga = GA(f)
-                        ga.run()
+                print(f)
+                ga = GA(f)
+                ga.run()
 
     '''Small_1 = [5,5,5,5,5,5]
     Small_2 = [5,5,5,10,10,10]
